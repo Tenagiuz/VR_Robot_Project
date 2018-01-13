0439c1331cb114866739420be369a1d5bd3b7115
matda <33860083+matda@users.noreply.github.com>
2018-01-13 21:36:04 +0100

    Add files via upload
    ho messo anche il link di dove ho trovato le info; non so se va bene  intanto ve lo linko

---
+++ come fare il contatore dei nemici.cs
@@ -0,0 +1,59 @@
+      #pragma strict
+      var health = 100;
+      var objectCounter : ObjectCounter;
+     
+     //Here's the important part:
+      function Start()
+      {
+          objectCounter = GameObject.Find("PaperCounter").GetComponent("ObjectCounter");
+      }
+      
+      function ApplyDamage (theDamage : int)
+      {
+          health -= theDamage;
+          
+          if (health <= 0)
+          {
+              Dead();
+          }
+      }
+      
+      function Dead()
+      {
+          objectCounter.Paper += 1;
+          Destroy (gameObject);
+      }
+	  // da mettere dentro il codice dei bot nemici
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  // questo è lo script da fare a parte come game object a qnt ho capito
+	         #pragma strict
+       var sfxHit: AudioClip;
+       var objectCounter : ObjectCounter;
+       function Start()
+       {
+           objectCounter = GameObject.Find("PaperCounter").GetComponent("ObjectCounter");
+       }
+       function OnTriggerEnter( other : Collider )
+       {
+           if (other.gameObject.tag == "Paper")
+           {
+               objectCounter.Paper += 1;
+               Debug.Log("A paper was picked up. Total papers = " + Paper);
+               Destroy(other.gameObject);
+               GetComponent.<AudioSource>().PlayOneShot(sfxHit);
+           }
+       }
+	   
+	   
+	   
+	   
+	   
+	   // https://answers.unity.com/questions/1154536/killed-enemies-counter.html
